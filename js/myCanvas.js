9966eeca53defafa39d89f4ecf659c763afeedde
Kin mi <backtothenearfuture@gmail.com>
2018-12-18 23:44:22 +0900

    set grid

--- js/myCanvas.js
+++ js/myCanvas.js
@@ -2,10 +2,30 @@ class baseCanvas {
     constructor(id) {
         this.id = id
     }
-    getCanvas() {
+    init() {
         this.canvas= document.getElementById(this.id);
-    }
-    getCtx() {
         this.ctx=this.canvas.getContext('2d');
+    }
+}
+
+class baseLayer extends baseCanvas{
+    constructor(id) {
+        super(id);
+        super.init();
+    }
+    drawGrid(width, height) {
+        this.ctx.strokeStyle='#000';
+        this.ctx.lineWidth=0.1;
+        //this.ctx.setLineDash([1,2]);
+        
+        for (var x = 0.5; x < width; x += 5) {
+            this.ctx.moveTo(x, 0);
+            this.ctx.lineTo(x, height);
+        }
+        for (var y = 0.5; y < height; y += 5) {
+            this.ctx.moveTo(0, y);
+            this.ctx.lineTo(width, y);
+        }
+        this.ctx.stroke();
     }
 }

120e8a2ba0e14b6f88fb3e6894429fc37bf6f979
Kin mi <backtothenearfuture@gmail.com>
2018-12-20 00:53:24 +0900

    draw 2d rect

--- js/myCanvas.js
+++ js/myCanvas.js
@@ -1,3 +1,25 @@
+const mWidth=800;
+const mHeight=600;
+
+$(function() {
+    $('#base-layer').attr({
+        width: mWidth,
+        height: mHeight
+    });
+    $('#layer1').attr({
+        width: mWidth,
+        height: mHeight
+    });
+
+    var base = new baseLayer("base-layer");
+    base.drawGrid(mWidth, mHeight);
+    var layer1 = new cubelayer("layer1");
+    $('#vertical, #horizontal, #height').on('input' , function(event) {
+        layer1.drawCube($('#vertical').val(), $('#horizontal').val(),$('#height').val());
+    })
+  });
+
+  
 class baseCanvas {
     constructor(id) {
         this.id = id
@@ -12,12 +34,10 @@ class baseLayer extends baseCanvas{
     constructor(id) {
         super(id);
         super.init();
-    }
-    drawGrid(width, height) {
         this.ctx.strokeStyle='#000';
         this.ctx.lineWidth=0.1;
-        //this.ctx.setLineDash([1,2]);
-        
+    }
+    drawGrid(width, height) {
         for (var x = 0.5; x < width; x += 5) {
             this.ctx.moveTo(x, 0);
             this.ctx.lineTo(x, height);
@@ -27,5 +47,21 @@ class baseLayer extends baseCanvas{
             this.ctx.lineTo(width, y);
         }
         this.ctx.stroke();
+    }
+}
+
+class cubelayer extends baseCanvas{
+    constructor(id) {
+        super(id);
+        super.init();
+        this.ctx.strokeStyle="rgba(0, 0, 0, 1.0)";
+        this.ctx.fillStyle='rgba(0, 100, 0, 0.5)';
+    }
+    drawCube(width,length,height){
+        var cWidth = this.canvas.clientWidth;
+        var cHeight = this.canvas.clientHeight;
+        this.ctx.clearRect(0, 0, cWidth, cHeight);
+        this.ctx.fillRect(10, cHeight - length - 10, width, length);
+        this.ctx.strokeRect(10, cHeight - length - 10, width, length);
     }
 }
